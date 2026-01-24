64560826fff9ea51350f0d93d4a91c329956e57c
nestoryGitPage <dcsccasual@dsmshipchandelling.com>
2026-01-24 17:45:47 +0300

    lattest new edits

---
+++ JSobject.js
@@ -0,0 +1,29 @@
+const projectNo = {
+  "24120022": {
+    Vessel_name: "MV Explorer",
+    Client: "MeTl",
+    startDate: "01.01.2025"
+      },
+      "26010017": {
+    Vessel_name: "MV neweer nn",
+    Client: "TFC",
+    startDate: "02.03.2025"
+      }
+};
+
+
+/////
+/*
+{
+  "25010001": {
+    "Vessel_name": "MV Explorer",
+    "Client": "MeTl",
+    "startDate": "01.01.2025"
+  },
+  "25010002": {
+    "Vessel_name": "MV JAXVAT",
+    "Client": "TFC",
+    "startDate": "02.03.2025"
+  }
+}
+*/
