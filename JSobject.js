38b9ef5e3c2b649eefcc5a7e9333e091da256301
nestory-mahunja <91778009+nestory-mahunja@users.noreply.github.com>
2026-02-07 19:39:08 +0300

    align top divs

--- JSobject.js
+++ JSobject.js
@@ -1,13 +1,33 @@
 const projectNo = {
   "24120022": {
-    Vessel_name: "MV Explorer",
-    Client: "MeTl",
-    startDate: "01.01.2025"
+    Vessel_Name: "MV",
+    Product: "Fertilizer)",
+    Client: "",
+    startDate: "31-12-2025"
       },
-      "26010017": {
-    Vessel_name: "MV neweer nn",
+      "25120027": {
+   Vessel_Name: "MV OBE ODYSSEY",
+    Product: "Fertilizer(P.Urea)",
+    Client: "Masmos",
+    startDate: "31-12-2025"
+      },
+      "25120054": {
+   Vessel_Name: "MV OBE ODYSSEY",
+    Product: "Fertilizer(P.Urea)",
+    Client: "Masmos",
+    startDate: "31-12-2025"
+      },
+      "26010029": {
+   Vessel_Name: "MV NORD SANTOS",
+    Product: "Fertilizer(DAP)",
+    Client: "OCP",
+    startDate: "31-12-2025"
+      },
+      "26010082": {
+   Vessel_Name: "MV NORD SANTOS",
+    Product: "Fertilizer(DAP)",
     Client: "TFC",
-    startDate: "02.03.2025"
+    startDate: "31-12-2025"
       }
 };
 
